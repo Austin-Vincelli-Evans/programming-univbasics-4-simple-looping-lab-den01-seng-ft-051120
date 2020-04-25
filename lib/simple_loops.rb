1b873379392740ab6e65680cc910c686deaabfe2
Austin Evans <unity7591@gmail.com>
2020-04-25 21:18:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,3 +13,11 @@ def loop_message_n_times(message, i)
     counter += 1
   end
 end
+
+def output_array(array)
+  counter = 0 
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+  end
+end
