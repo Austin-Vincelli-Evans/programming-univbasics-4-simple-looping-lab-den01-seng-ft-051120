770a48e3f02b98d44546fb9e4783836fd5373723
Austin Evans <unity7591@gmail.com>
2020-04-25 21:15:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,7 @@
-def loop_message_five_times(string)
+def loop_message_five_times(message)
   counter=0 
-  while [counter<5] do
-    puts (string)
+  while counter<5 do
+    puts message
     counter += 1
   end
 end
